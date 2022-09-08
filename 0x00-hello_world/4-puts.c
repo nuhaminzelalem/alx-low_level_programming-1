3286eeba0caa18b5c319c1ae33afb40e6f962ff0
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-08 14:29:00 -0700

    C languge with put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
-/**	
- * main: void return at entry point
-*
-*  Return: 0 after printing the  function
-*
-*/	
-	int main(void)
-	 
+
+/**
+ *
+ *  main -entry point
+ *
+ *  
+ *
+ *    Return: 0 after printing the puts  function
+ *
+ */
+
+int main(void)
 {
+
+
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+
 }
