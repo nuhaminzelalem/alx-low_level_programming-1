be15eb8156ea44d764d52e7a527753293175c9e6
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-08 13:12:57 -0700

    C programming using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/* c programming that
+ * prints the prescriped sentence 
+ * using puts function
+ */
+int main (void){
+
+puts("\"Programming is like building a multilingual puzzle,\n");
+return(0);
+}
