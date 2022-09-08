0daf4f9cfa30dfb9df28d297cf0a7800eba38752
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-08 13:24:58 -0700

    C programming using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
+
 /* 
  * c programming that
+ *
  * prints the prescriped sentenc
  *  
- * using puts function
- *
+ * 
  * main -entry point
  *
- * return 0
+ * Return: 0 after printing the  function
+ *
  */
-int main(void){
+int main(void)
+{
 
-puts("\"Programming is like building a multilingual puzzle \n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
