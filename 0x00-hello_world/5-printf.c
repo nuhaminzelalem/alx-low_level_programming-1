e00e67c8719056e3747ec646055c317bd867b10c
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-08 15:04:42 -0700

    C languge that print with printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main -print function with printf funtion
+ *
+ * Return :no return function
+ *
+ */
+	int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
