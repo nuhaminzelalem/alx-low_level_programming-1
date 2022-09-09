2e74ecf90fe6332226114e8b8b3ee13bea73140d
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-09 14:41:13 -0700

    it prints the last digit and decide less than or greater than 5,6

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,12 +7,11 @@
  */
 int main(void)
 {
-	int n;
-	int a;
+	int n, a;
 
 	srand(time(0));
 	n = rand() - RAND_MAX /2;
-	a = n / 2;
+	a = n % 10;
 
 	if (a > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, a);
