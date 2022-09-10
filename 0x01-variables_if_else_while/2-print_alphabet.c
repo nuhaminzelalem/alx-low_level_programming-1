54f7bd38d2e7d4c41cd9c64070295b339f6c19ce
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-10 00:26:06 -0700

    Prints lower case alphabet using putchar functionn

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabel in lower case using putchar function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower_alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (i < 26)
+	{
+		putchar(lower_alpha[i];
+	}
+	putchar("\n");
+}
