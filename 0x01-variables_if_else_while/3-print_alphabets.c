4c92f707eab3a633f47add49add02e25886cb200
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-10 00:43:47 -0700

    print lower and upper alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * main - prints lower case and upper case alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph_low_upper[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i = 0;
+
+	while (i < 52)
+	{
+		putchar(alph_low_upper[i]);
+		i++;
+	}
+	putchar('/n');
+	return (0);
+}
