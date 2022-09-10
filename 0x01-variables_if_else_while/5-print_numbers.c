3bf7baf54f73d9faa749dc67b011e5141d8bc178
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-10 01:29:56 -0700

    print digits from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
