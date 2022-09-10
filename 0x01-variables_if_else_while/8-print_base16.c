e9be424f5f3faa7b3ff80708bb9a6b047d7795d8
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-10 01:56:00 -0700

    print lowercase alphabet in reverse order

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 103)
+	{
+		if (!(i > 58 && i < 96))
+		{
+			putchar(i);
+			i++;
+		}
+		putchar('\n');
+		return (0);
+	}
+}
