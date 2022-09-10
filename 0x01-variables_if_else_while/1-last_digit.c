f80fc196801a1c0ab8a11aa5d2f5555e84db8c4f
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-10 00:04:48 -0700

    print the last digit and decide less or greater than 5,6, and 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - last digit is greater than 5, is less than 6, or is 0
+ * main - print digit is greater than 5, is less than 6, or is 0
  * Return : Always 0 (Success)
  */
 int main(void)
