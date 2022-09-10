2d44c741486b1716cd4fa6be727c1542b91fd9b2
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-09 23:06:02 -0700

    print the last digit and decide less or greater than 5,6, and 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,15 +21,15 @@ int main(void)
 	}
 	else if (a == 0)
 	{
-
+		
 		printf("Last digit of %d is %d and is 0\n", n, a);
-
+	
 	}
 	else
-	{	
-
+	{
+		
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
-
+	
 	}
 
 	return (0);
