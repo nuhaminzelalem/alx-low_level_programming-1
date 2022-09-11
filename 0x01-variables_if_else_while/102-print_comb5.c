1f443ab0d36afd240737092705c977ba4ae78618
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-11 11:56:13 -0700

    prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,38 +8,42 @@ int main(void)
 	int i = 48;
 	int j = 48;
 	int x = 48;
-	int y = 49;
+	int y = 48;
 
 	while (i < 58)
 	{
+		j = 48;
+
 		while (j < 57)
 		{
+			x = 48;
+
 			while (x < 58)
 			{
+				y = 48;
+
 				while (y < 58)
 				{
+					if ((i+j) < (x+y))
+					{
 				putchar(i);
 				putchar(j);
 				putchar(32);
 				putchar(x);
 				putchar(y);
-				if (!(i == 57 && j == 56))
+					}
+				if (!(i == 57 && j == 56 && x =57 && y = 57))
 				{
 					putchar(44);
+					putchar(32);
 				}
 				y++;
 				}
 				x++;
-				y = 48;
 			}
 			j++;
-			x = 48;
-			y = 48;
 		}
-		i++;
-		x = 48;
-		y = 48;
-		j = 48;
+		i++
 	}
 	putchar('\n');
 	return (0);
