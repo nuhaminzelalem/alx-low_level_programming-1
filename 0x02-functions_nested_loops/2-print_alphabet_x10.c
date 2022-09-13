d304438d7a78bb48d65bd8ff2c2e24fede15bf5a
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-13 11:29:42 -0700

    prints 10 times the alphabet, in lowercase,

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -  prints 10 times the alphabet, in lowercase
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char alpha[]="abcdefghijklmnopqrstuvwxyz";
+	int i, j;
+
+	for (i = 1; i < 11; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(alpha[j];
+		}
+		_putchar('\n'');
+	}
+}
