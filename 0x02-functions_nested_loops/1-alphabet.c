b49c1f39a1ef7efdd312291c9bb5b96f02ec79ea
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-13 10:35:59 -0700

    prints the alphabet, in lowercase,

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,8 +3,7 @@
  * main - prints the alphabet, in lowercase, followed by a new line.
  * Return: Always 0 (Success)
  */
-void print_alphabet();
-int main(void)
+void print_alphabet()
 {
 	char alpha[ ] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
