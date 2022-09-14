6adb62ea4509fe3651de9266d65c12ed692fdfa0
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-14 13:16:03 -0700

    function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,6 +32,7 @@ void print_times_table(int n)
 						continue;
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
 				}
 				else
 				{
@@ -40,6 +41,7 @@ void print_times_table(int n)
 					if (j == n)
 						continue;
 					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 				}
 			}
