0c95fc7babfe832c46af269f0249fa30740b9805
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-15 09:23:37 -0700

    function that prints the fuzz buzz game

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - a program that prints the numbers from 1 to 100, followed by a new line.
+ * main - a program that prints the numbers from 1 to 100.
  * Return: Always 0 (Success)
  */
 int main(void)
