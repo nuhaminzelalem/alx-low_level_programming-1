eef9de92b0a8d9cd49096ea2e22f2b01e707bfe6
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-14 22:51:20 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,11 +10,7 @@ char word[] = "_putchar";
 
 	for (i = 0; i < 8 ; i++)
 	{
-<<<<<<< HEAD
 		_putchar(word[i]);
-=======
-		_putchar('word[i]');
->>>>>>> d5147f48088e96f675831df8f2161401247d8f9d
 	}
 	_putchar('\n');
 	return (0);
